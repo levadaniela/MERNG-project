a64800b2017c039b90a5c726fd8e9cc1c88b8d89
Dana <leva.daniela@yahoo.com>
2021-04-15 21:10:53 +0100

    +comments, likes, subscription

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require("apollo-server");
 
 const Post = require("../../models/Post");
 const checkAuth = require("../../util/check-auth");
@@ -36,6 +36,11 @@ module.exports = {
         createdAt: new Date().toISOString(),
       });
       const post = await newPost.save();
+
+      context.pubsub.publish("NEW_POST", {
+        newPost: post,
+      });
+
       return post;
     },
     async deletePost(_, { postId }, context) {
@@ -52,6 +57,29 @@ module.exports = {
       } catch (err) {
         throw new Error(err);
       }
+    },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          //Post already likes, unlike it
+          post.likes = post.likes.filter((like) => like.username !== username);
+        } else {
+          //Not liked, like post
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString(),
+          });
+        }
+        await post.save();
+        return post;
+      } else throw new UserInputError("Post not found");
+    },
+  },
+  Subscription: {
+    newPost: {
+      subscribe: (_, __, { pubsub }) => pubsub.asyncIterator("NEW_POST"),
     },
   },
 };
